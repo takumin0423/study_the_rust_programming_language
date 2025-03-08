9bc806c52e57b09c3ba992d20362680f1431741e
takumin0423 <takumin0423baske@gmail.com>
2025-03-09 00:31:59 +0900

    feat: 🎸 Data Types

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,70 @@
+use std::io;
+
 fn main() {
-    let x = 5;
+    let guess: u32 = "42".parse().expect("Not a number!");
 
-    let x = x + 1;
+    let x = 2.0;
+    let y: f32 = 3.0;
 
-    {
-        let x = x * 2;
+    let sum = 5 + 10;
+    let difference = 95.9 - 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3;
+    let remainder = 43 % 5;
 
-        println!("The value of x in the inner scope is: {x}");
-    }
+    let c = 'z';
+    let x: char = 'ℤ';
+    let heart_eyed_cat = '😻';
 
-    println!("The value of x is: {x}");
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {y}");
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let five_hundred = x.0;
+    let six_point_four = x.1;
+    let one = x.2;
+
+    let a = [1, 2, 3, 4, 5];
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let a = [3; 5];
+    let a = [1, 2, 3, 4, 5];
+    let first = a[0];
+    let second = a[1];
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
-
-const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
