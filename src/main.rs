74a8aee99b8c6ca6844775cde311484b189fe3c1
takumin0423 <takumin0423baske@gmail.com>
2025-03-09 02:33:47 +0900

    feat: 🎸 Functions

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,36 @@
-use std::io;
-
 fn main() {
-    let guess: u32 = "42".parse().expect("Not a number!");
+    another_function(5);
+    print_labeled_measurement(5, 'h');
 
-    let x = 2.0;
-    let y: f32 = 3.0;
+    let y = {
+        let x = 3;
 
-    let sum = 5 + 10;
-    let difference = 95.9 - 4.3;
-    let product = 4 * 30;
-    let quotient = 56.7 / 32.2;
-    let truncated = -5 / 3;
-    let remainder = 43 % 5;
-
-    let c = 'z';
-    let x: char = 'ℤ';
-    let heart_eyed_cat = '😻';
-
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
-    let tup = (500, 6.4, 1);
-    let (x, y, z) = tup;
+        x + 1
+    };
 
     println!("The value of y is: {y}");
 
-    let x: (i32, f64, u8) = (500, 6.4, 1);
-    let five_hundred = x.0;
-    let six_point_four = x.1;
-    let one = x.2;
+    let x = five();
 
-    let a = [1, 2, 3, 4, 5];
-    let months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ];
-    let a: [i32; 5] = [1, 2, 3, 4, 5];
-    let a = [3; 5];
-    let a = [1, 2, 3, 4, 5];
-    let first = a[0];
-    let second = a[1];
+    println!("The value of x is: {x}");
 
-    let a = [1, 2, 3, 4, 5];
+    let x = plus_one(5);
 
-    println!("Please enter an array index.");
+    println!("the value of x is: {x}");
+}
 
-    let mut index = String::new();
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
 
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
 
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Index entered was not a number");
+fn five() -> i32 {
+    5
+}
 
-    let element = a[index];
-
-    println!("The value of the element at index {index} is: {element}");
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
