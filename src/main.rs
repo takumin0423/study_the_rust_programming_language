b18eb3b4426c12fa670824d1977227a07ebd4e9d
takumin0423 <takumin0423baske@gmail.com>
2025-03-10 02:23:42 +0900

    feat: 🎸 Storing Lists of Values with Vectors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,42 @@
 fn main() {
-    let config_max = Some(3u8);
+    let x: Vec<i32> = Vec::new();
+    let v = vec![1, 2, 3];
 
-    if let Some(max) = config_max {
-        println!("The maximum is configured to be {max}");
+    let mut v = Vec::new();
+
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element."),
     }
+
+    let mut v = vec![100, 32, 57];
+
+    for i in &v {
+        println!("{i}");
+    }
+
+    for i in &mut v {
+        *i += 50;
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+}
+
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
 }
