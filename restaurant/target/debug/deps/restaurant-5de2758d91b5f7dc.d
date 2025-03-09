c6a08bd324d91f4b3e9c65f1e6937f9c89c60317
takumin0423 <takumin0423baske@gmail.com>
2025-03-10 00:19:04 +0900

    feat: 🎸 Defining Modules to Control Scope and Privacy

---
+++ restaurant/target/debug/deps/restaurant-5de2758d91b5f7dc.d
@@ -0,0 +1,5 @@
+/Users/takumi.igusa/Documents/rust/study_the_rust_programming_language/restaurant/target/debug/deps/librestaurant-5de2758d91b5f7dc.rmeta: src/lib.rs
+
+/Users/takumi.igusa/Documents/rust/study_the_rust_programming_language/restaurant/target/debug/deps/restaurant-5de2758d91b5f7dc.d: src/lib.rs
+
+src/lib.rs:
---
+++ restaurant/target/debug/deps/restaurant-d3f33dde6e68f7f4.d
@@ -0,0 +1,5 @@
+/Users/takumi.igusa/Documents/rust/study_the_rust_programming_language/restaurant/target/debug/deps/librestaurant-d3f33dde6e68f7f4.rmeta: src/lib.rs
+
+/Users/takumi.igusa/Documents/rust/study_the_rust_programming_language/restaurant/target/debug/deps/restaurant-d3f33dde6e68f7f4.d: src/lib.rs
+
+src/lib.rs:
