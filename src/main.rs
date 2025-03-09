75759cf5f5f35ba9ea5bdf4e0404db184f613f93
takumin0423 <takumin0423baske@gmail.com>
2025-03-09 11:45:59 +0900

    feat: 🎸 References and Borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,35 @@
 fn main() {
-    let s = "hello";
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+
+    println!("The length of {s1} is {len}");
 
     let mut s = String::from("hello");
 
-    s.push_str(", world!");
-
-    println!("{s}");
-
-    let x = 5;
-    let y = x;
-
-    let s1 = String::from("hello");
-    let s2 = s1;
+    change(&mut s);
 
     let mut s = String::from("hello");
-    s = String::from("ahoy");
 
-    println!("{s}, world!");
+    let r1 = &s;
+    let r2 = &s;
 
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
+    println!("{r1} and {r2}");
 
-    println!("s1 = {s1}, s2 = {s2}");
+    let r3 = &mut s;
 
-    let x = 5;
-    let y = x;
+    println!("{r3}");
+}
 
-    println!("x = {x}, y = {y}");
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
 
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn no_dangle() -> String {
     let s = String::from("hello");
 
-    takes_ownership(s);
-
-    let x = 5;
-
-    makes_copy(x);
-
-    println!("{}", x);
-
-    let s1 = gives_ownership();
-    let s2 = String::from("hello");
-    let s3 = takes_and_gives_back(s2);
-}
-
-fn takes_ownership(some_string: String) {
-    println!("{some_string}");
-}
-
-fn makes_copy(some_integer: i32) {
-    println!("{some_integer}");
-}
-
-fn gives_ownership() -> String {
-    let some_string = String::from("yours");
-
-    some_string
-}
-
-fn takes_and_gives_back(a_string: String) -> String {
-    a_string
+    s
 }
