4615e5b5ffa0017553b5323f27e677cb271651df
takumin0423 <takumin0423baske@gmail.com>
2025-03-09 23:25:20 +0900

    feat: 🎸 The match Control Flow Construct

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,49 @@
-use std::net::{Ipv4Addr, Ipv6Addr};
-
 fn main() {
-    let home = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
-    let loopback = IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1));
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 
-    let m = Message::Write(String::from("hello"));
-    m.call();
+    let dice_roll = 9;
 
-    let some_number = Some(5);
-    let some_char = Some('e');
-    let absent_number: Option<i32> = None;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => (),
+    }
 }
 
-enum IpAddr {
-    V4(Ipv4Addr),
-    V6(Ipv6Addr),
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
 }
 
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
 }
 
-impl Message {
-    fn call(&self) {}
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+
+            25
+        }
+    }
 }
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
