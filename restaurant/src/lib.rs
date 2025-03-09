ae71c9933f1c39f4b2de20a897d84e8442521e49
takumin0423 <takumin0423baske@gmail.com>
2025-03-10 00:53:48 +0900

    feat: 🎸 Separating Modules into Different Files

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,48 +1,7 @@
+mod front_of_house;
+
 pub use crate::front_of_house::hosting;
-use std::collections::HashMap;
-
-fn main() {
-    let mut map = HashMap::new();
-
-    map.insert(1, 2);
-}
-
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-    }
-}
 
 pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
-}
-
-fn deliver_order() {}
-
-mod back_of_house {
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-
-    fn fix_incorrect_order() {
-        cook_order();
-        super::deliver_order();
-    }
-
-    fn cook_order() {}
-
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
 }
--- restaurant/target/debug/deps/restaurant-5de2758d91b5f7dc.d
+++ restaurant/target/debug/deps/restaurant-5de2758d91b5f7dc.d
@@ -1,5 +1,7 @@
-/Users/takumi.igusa/Documents/rust/study_the_rust_programming_language/restaurant/target/debug/deps/librestaurant-5de2758d91b5f7dc.rmeta: src/lib.rs
+/Users/takumi.igusa/Documents/rust/study_the_rust_programming_language/restaurant/target/debug/deps/librestaurant-5de2758d91b5f7dc.rmeta: src/lib.rs src/front_of_house.rs src/front_of_house/hosting.rs
 
-/Users/takumi.igusa/Documents/rust/study_the_rust_programming_language/restaurant/target/debug/deps/restaurant-5de2758d91b5f7dc.d: src/lib.rs
+/Users/takumi.igusa/Documents/rust/study_the_rust_programming_language/restaurant/target/debug/deps/restaurant-5de2758d91b5f7dc.d: src/lib.rs src/front_of_house.rs src/front_of_house/hosting.rs
 
 src/lib.rs:
+src/front_of_house.rs:
+src/front_of_house/hosting.rs:
