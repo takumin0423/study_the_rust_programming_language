69fe9a8ab1b254972464b9109d655e7b2ab68618
takumin0423 <takumin0423baske@gmail.com>
2025-03-09 22:15:52 +0900

    feat: 🎸 Defining and Instantiating Structs

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,41 @@
 fn main() {
-    let mut s = String::from("hello world");
-    let word = first_word(&s);
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
 
-    println!("the first word is: {word}");
+    user1.email = String::from("anotheremaill@example.com");
 
-    let my_string = String::from("hello world");
-    let word = first_word(&my_string[0..6]);
-    let word = first_word(&my_string[..]);
-    let word = first_word(&my_string);
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
 
-    let my_string_literal = "hello world";
-    let word = first_word(&my_string_literal[0..6]);
-    let word = first_word(&my_string_literal[..]);
-    let word = first_word(my_string_literal);
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 
-    let a = [1, 2, 3, 4, 5];
-    let slice = &a[1..3];
-    assert_eq!(slice, &[2, 3]);
+    let subject = AlwaysEqual;
 }
 
-fn first_word(s: &str) -> &str {
-    let bytes = s.as_bytes();
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
 
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[0..i];
-        }
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
     }
-
-    &s[..]
 }
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+struct AlwaysEqual;
