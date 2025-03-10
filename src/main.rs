9e50e86a4193f1359bd73561dbd905888be46e48
takumin0423 <takumin0423baske@gmail.com>
2025-03-10 23:40:27 +0900

    feat: 🎸 To panic! or Not to panic!

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,19 @@
-use std::{
-    error::Error,
-    fs::{self, File},
-    io::{self, ErrorKind, Read},
-};
+fn main() {}
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let greeting_file_result = File::open("hello.txt");
-
-    let greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file: {e:?}"),
-            },
-            other_error => {
-                panic!("Problem opening the file: {other_error:?}");
-            }
-        },
-    };
-
-    let other_file = File::open("hello.txt").unwrap();
-    let other_file = File::open("hello.txt").expect("hello.txt should be included in this project");
-
-    Ok(())
+pub struct Guess {
+    value: i32,
 }
 
-fn read_username_from_file() -> Result<String, io::Error> {
-    let username_file_result = File::open("hello.txt");
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {value}.")
+        }
 
-    let mut username_file = match username_file_result {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
-
-    let mut username = String::new();
-
-    match username_file.read_to_string(&mut username) {
-        Ok(_) => Ok(username),
-        Err(e) => Err(e),
+        Guess { value }
     }
-}
 
-fn test() -> Result<String, io::Error> {
-    fs::read_to_string("hello.txt")
-}
-
-fn last_char_of_first_line(text: &str) -> Option<char> {
-    text.lines().next()?.chars().last()
+    pub fn value(&self) -> i32 {
+        self.value
+    }
 }
