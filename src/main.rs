b7b68a54b7a76cc4e8f02417820c6916d429ba8d
takumin0423 <takumin0423baske@gmail.com>
2025-03-10 22:56:28 +0900

    feat: 🎸 Recoverable Errors with Result

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,51 @@
-fn main() {
-    // panic!("crash and burn");
+use std::{
+    error::Error,
+    fs::{self, File},
+    io::{self, ErrorKind, Read},
+};
 
-    let v = vec![1, 2, 3];
+fn main() -> Result<(), Box<dyn Error>> {
+    let greeting_file_result = File::open("hello.txt");
 
-    v[99];
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {e:?}"),
+            },
+            other_error => {
+                panic!("Problem opening the file: {other_error:?}");
+            }
+        },
+    };
+
+    let other_file = File::open("hello.txt").unwrap();
+    let other_file = File::open("hello.txt").expect("hello.txt should be included in this project");
+
+    Ok(())
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn test() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }
