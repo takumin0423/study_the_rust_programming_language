02f9e615689abb12701b8517fea344d9483609d9
takumin0423 <takumin0423baske@gmail.com>
2025-03-10 22:17:02 +0900

    feat: 🎸 Unrecoverable Errors with panic!

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,7 @@
-use std::collections::HashMap;
-
 fn main() {
-    let mut scores = HashMap::new();
+    // panic!("crash and burn");
 
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
+    let v = vec![1, 2, 3];
 
-    let team_name = String::from("blue");
-    let score = scores.get(&team_name).copied().unwrap_or(0);
-
-    for (key, value) in &scores {
-        println!("{key}: {value}");
-    }
-
-    let field_name = String::from("Favorite color");
-    let field_value = String::from("Blue");
-    let mut map = HashMap::new();
-
-    map.insert(field_name, field_value);
-
-    scores.entry(String::from("Yellow")).or_insert(50);
-
-    let text = "hello world wonderful world";
-    let mut map = HashMap::new();
-
-    for word in text.split_whitespace() {
-        let count = map.entry(word).or_insert(0);
-
-        *count += 1;
-    }
-
-    println!("{map:?}");
+    v[99];
 }
