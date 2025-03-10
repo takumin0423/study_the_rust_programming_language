a58a5b26a2ad6ed463074e454e4e390bbfadb6f4
takumin0423 <takumin0423baske@gmail.com>
2025-03-10 19:17:37 +0900

    feat: 🎸 Storing UTF-8 Encoded Text with Strings

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,9 @@ fn main() {
     let mut s = "initial contents".to_string();
 
     s.push_str("bar");
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{s1}-{s2}-{s3}");
 }
