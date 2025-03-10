df1a70b6c506aea5e58608d8fa7a61126227f6f2
takumin0423 <takumin0423baske@gmail.com>
2025-03-11 01:32:28 +0900

    feat: 🎸 Validating References with Lifetimes

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,60 @@
-use std::fmt::{Debug, Display};
+use std::fmt::Display;
 
 fn main() {
-    let tweet = Tweet {
-        username: String::from("hourse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        retweet: false,
+    let string1 = String::from("long string is long");
+    let result;
+
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+    }
+
+    println!("The longest string is {result}");
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().unwrap();
+    let i = ImportantExcerpt {
+        part: first_sentence,
     };
-
-    println!("1 new tweet: {}", tweet.summarize());
-
-    let article = NewsArticle {
-        headline: String::from("Penguins win the Stanley Cup Championship!"),
-        location: String::from("Pittsburgh, PA, USA"),
-        author: String::from("Iceburgh"),
-        content: String::from(
-            "The Pittsburgh Penguins once again are the best \
-             hockey team in the NHL.",
-        ),
-    };
-
-    println!("New article available! {}", article.summarize());
 }
 
-pub trait Summary {
-    fn summarize_author(&self) -> String;
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() { x } else { y }
+}
 
-    fn summarize(&self) -> String {
-        format!("(Read more from {}...)", self.summarize_author())
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {announcement}");
+
+        self.part
     }
 }
 
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
+fn first_word<'a>(s: &'a str) -> &'a str {
+    let bytes = s.as_bytes();
 
-impl Summary for NewsArticle {
-    fn summarize_author(&self) -> String {
-        format!("Test")
-    }
-}
-
-struct Tweet {
-    username: String,
-    content: String,
-    reply: bool,
-    retweet: bool,
-}
-
-impl Summary for Tweet {
-    fn summarize_author(&self) -> String {
-        format!("@{}", self.username)
-    }
-}
-
-fn notify<T: Summary + Display>(item: &T) {
-    println!("test")
-}
-
-fn some_function<T, U>(t: &T, u: &U) -> i32
-where
-    T: Display + Clone,
-    U: Clone + Debug,
-{
-    3
-}
-
-fn test() -> impl Summary {}
-
-struct Pair<T> {
-    x: T,
-    y: T,
-}
-
-impl<T> Pair<T> {
-    fn new(x: T, y: T) -> Self {
-        Self { x, y }
-    }
-}
-
-impl<T: Display + PartialOrd> Pair<T> {
-    fn cmp_display(&self) {
-        if self.x >= self.y {
-            println!("test")
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
         }
     }
+
+    &s[..]
+}
+
+fn longest_with_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {ann}");
+
+    if x.len() > y.len() { x } else { y }
 }
