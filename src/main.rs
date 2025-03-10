61b9fd7a5743ade632b975cfca685336ffd195c9
takumin0423 <takumin0423baske@gmail.com>
2025-03-11 00:22:02 +0900

    feat: 🎸 Generic Data Types

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,49 @@
-fn main() {}
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
 
-pub struct Guess {
-    value: i32,
-}
+    let mut largest = &number_list[0];
 
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {value}.")
+    for number in &number_list {
+        if number > largest {
+            largest = number;
         }
-
-        Guess { value }
     }
 
-    pub fn value(&self) -> i32 {
-        self.value
+    println!("The largest number is {largest}");
+
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+
+    let p = Point { x: 5, y: 10 };
+
+    println!("p.x = {}", p.x());
+}
+
+fn largest<T>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
     }
 }
