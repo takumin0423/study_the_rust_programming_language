8b4a87ea1223a7734a84d69774386e7a108887d0
takumin0423 <takumin0423baske@gmail.com>
2025-03-11 21:31:57 +0900

    feat: 🎸 guessing_game

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,8 @@ use std::fmt::Display;
 
 fn main() {
     let string1 = String::from("long string is long");
-    let result;
-
-    {
-        let string2 = String::from("xyz");
-        result = longest(string1.as_str(), string2.as_str());
-    }
-
-    println!("The longest string is {result}");
 
     let novel = String::from("Call me Ishmael. Some years ago...");
-    let first_sentence = novel.split('.').next().unwrap();
-    let i = ImportantExcerpt {
-        part: first_sentence,
-    };
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
