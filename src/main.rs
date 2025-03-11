ef3d97a51a08209eca437240594340c625fc3851
takumin0423 <takumin0423baske@gmail.com>
2025-03-12 01:31:31 +0900

    feat: 🎸 How to Write Tests

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,118 @@
-use std::fmt::Display;
+fn main() {}
 
-fn main() {
-    let string1 = String::from("long string is long");
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let novel = String::from("Call me Ishmael. Some years ago...");
-}
+    #[test]
+    fn it_works() -> Result<(), String> {
+        let result = add(2, 2);
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() { x } else { y }
-}
-
-struct ImportantExcerpt<'a> {
-    part: &'a str,
-}
-
-impl<'a> ImportantExcerpt<'a> {
-    fn level(&self) -> i32 {
-        3
-    }
-
-    fn announce_and_return_part(&self, announcement: &str) -> &str {
-        println!("Attention please: {announcement}");
-
-        self.part
-    }
-}
-
-fn first_word<'a>(s: &'a str) -> &'a str {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[0..i];
+        if result == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
         }
     }
 
-    &s[..]
+    #[test]
+    fn exploration() {
+        let result = add(2, 2);
+
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        let result = add_two(2);
+
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{result}`"
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
 }
 
-fn longest_with_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
-where
-    T: Display,
-{
-    println!("Announcement! {ann}");
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
 
-    if x.len() > y.len() { x } else { y }
+pub fn add_two(a: usize) -> usize {
+    a + 3
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {name}!")
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {value}");
+        }
+
+        Guess { value }
+    }
 }
