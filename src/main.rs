30cbfd90f5a92d25af87838a23c87652539f2b5e
takumin0423 <takumin0423baske@gmail.com>
2025-03-22 20:05:50 +0900

    feat: closures: Anonymous Functions that Capture Their Environment

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,61 @@
-fn main() {}
+use std::thread;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
 
-    #[test]
-    fn it_works() -> Result<(), String> {
-        let result = add(2, 2);
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
 
-        if result == 4 {
-            Ok(())
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+        if num_red > num_blue {
+            ShirtColor::Red
         } else {
-            Err(String::from("two plus two does not equal four"))
+            ShirtColor::Blue
         }
     }
-
-    #[test]
-    fn exploration() {
-        let result = add(2, 2);
-
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn another() {
-        panic!("Make this test fail");
-    }
-
-    #[test]
-    fn larger_can_hold_smaller() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(larger.can_hold(&smaller));
-    }
-
-    #[test]
-    fn smaller_cannot_hold_larger() {
-        let larger = Rectangle {
-            width: 8,
-            height: 7,
-        };
-        let smaller = Rectangle {
-            width: 5,
-            height: 1,
-        };
-
-        assert!(!smaller.can_hold(&larger));
-    }
-
-    #[test]
-    fn it_adds_two() {
-        let result = add_two(2);
-
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn greeting_contains_name() {
-        let result = greeting("Carol");
-
-        assert!(
-            result.contains("Carol"),
-            "Greeting did not contain name, value was `{result}`"
-        );
-    }
-
-    #[test]
-    #[should_panic]
-    fn greater_than_100() {
-        Guess::new(200);
-    }
 }
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+fn main() {
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
 
-pub fn add_two(a: usize) -> usize {
-    a + 3
-}
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
 
-pub fn greeting(name: &str) -> String {
-    format!("Hello {name}!")
-}
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
 
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
 
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-}
-
-pub struct Guess {
-    value: i32,
-}
-
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {value}");
-        }
-
-        Guess { value }
-    }
+    thread::spawn(move || println!("From thread: {list:?}"))
+        .join()
+        .unwrap();
 }
